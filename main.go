8c1aa63189dfc4f7ecd874e9da2ef0c0fa5be6b2
kohrVid <kohrVid@zoho.com>
2019-11-17 14:51:01 +0000

    Add support for TLS to auth API
    It should be possible to authenticate with the server using the
    self-hosted OpenSSL certificate.

--- main.go
+++ main.go
@@ -4,29 +4,39 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"path/filepath"
 
+	certs "github.com/kohrVid/auth/certs/helpers"
 	"github.com/kohrVid/auth/proto"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 )
 
 type server struct{}
 
 func main() {
+	certPath := filepath.Join("..", "certs")
+	certFile, keyFile := certs.TlsCerts(certPath, "server.crt", "server.key")
+	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	if err != nil {
+		log.Fatalf("unable to construct TLS credentials from certificate: %v", err)
+	}
+
+	s := grpc.NewServer(grpc.Creds(creds))
 	lis, err := net.Listen("tcp", ":9999")
 	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
 
 	fmt.Println("Listening on localhost:9999...")
 
-	s := grpc.NewServer()
 	proto.RegisterAuthenticationServiceServer(s, &server{})
 	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
