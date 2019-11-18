747260efcd55e6083ec7ef41a96025aa18f4995a
kohrVid <kohrVid@zoho.com>
2019-11-18 08:56:31 +0000

    Add HealthCheck endpoint
    Have added a very basic healthcheck endpoint to the Routes package which
    should return a serving status if the application is running.

--- routes/routes.go
+++ routes/routes.go
@@ -12,3 +12,7 @@ func (s *Server) CredentialCheck(ctx context.Context, r *proto.AuthenticationReq
 	// TODO - Actually authenticate against a database
 	return &proto.AuthenticationResponse{Result: "OK"}, nil
 }
+
+func (s *Server) HealthCheck(ctx context.Context, r *proto.HealthCheckRequest) (*proto.HealthCheckResponse, error) {
+	return &proto.HealthCheckResponse{Status: 1}, nil
+}
--- routes/routes_test.go
+++ routes/routes_test.go
@@ -7,13 +7,18 @@ import (
 	"github.com/kohrVid/auth/proto"
 )
 
+var s Server = Server{}
+
 func TestCredentialsCheck(t *testing.T) {
-	s := Server{}
 	expectedResp := "OK"
 	username := "magda"
 	password := "magypi123"
 
-	req := &proto.AuthenticationRequest{Username: username, Password: password}
+	req := &proto.AuthenticationRequest{
+		Username: username,
+		Password: password,
+	}
+
 	resp, err := s.CredentialCheck(context.Background(), req)
 	if err != nil {
 		t.Errorf(
@@ -23,6 +28,27 @@ func TestCredentialsCheck(t *testing.T) {
 	}
 
 	if resp.Result != expectedResp {
+		t.Errorf(
+			"Test failed.\nGot:\n\t%v\nExpected:\n\t%v",
+			s,
+			expectedResp,
+		)
+	}
+}
+
+func TestHealthCheck(t *testing.T) {
+	var expectedResp proto.HealthCheckResponse_ServingStatus = 1
+	req := &proto.HealthCheckRequest{Service: "auth-api"}
+
+	resp, err := s.HealthCheck(context.Background(), req)
+	if err != nil {
+		t.Errorf(
+			"Test failed.\nGot:\n\t%v\n",
+			err,
+		)
+	}
+
+	if resp.Status != expectedResp {
 		t.Errorf(
 			"Test failed.\nGot:\n\t%v\nExpected:\n\t%v",
 			s,
